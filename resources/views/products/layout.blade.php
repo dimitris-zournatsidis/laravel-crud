e991dc85720467301079bb343ff12191fb07f327
Dimitris <jimelectrician@gmail.com>
2022-10-12 20:00:00 +0300

    basic crud operation done

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <title>Laravel CRUD Application</title>
+    <link 
+        rel="stylesheet" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" 
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" 
+        crossorigin="anonymous"
+    >
+</head>
+<body>
+    <h1 class="text-center my-3">Laravel CRUD Application</h1>
+    <div class="container">
+        <br>
+        @yield('content')
+    </div>
+    
+</body>
+</html>
